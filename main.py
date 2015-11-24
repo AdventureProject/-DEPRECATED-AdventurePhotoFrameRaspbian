3346ef3e906cdca6f017b08d0bd52d4a1930a595
Wavesonics <adamwbrown@gmail.com>
2015-11-23 17:57:15 -0800

    Lots of work, mainly it now resizes and then crops images so there is no distortion. Also it hits an end point specifically for the photoframe which returns photos that are just big enough (download times are much shorter now)
    Also rotate times are now 1 minute to help debug the sleep problem

--- main.py
+++ main.py
@@ -4,44 +4,50 @@ import sys
 import time
 import urllib, json
 import pygame
+from PIL import Image
+from resizeimage import resizeimage
 
 
-def downloadPhoto():
+def download_photo():
 	print('fetching data...')
-	url = "http://wethinkadventure.rocks/random"
+	url = "http://wethinkadventure.rocks/photoframe"
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
 
-	imageUrl = data['image']
+	image_url = data['image']
 
-	print(imageUrl)
+	print(image_url)
 	print('downloading image ...')
 
-	fileName = 'current.jpg'
-	f = open(fileName, 'wb')
-	f.write(urllib.urlopen(imageUrl).read())
+	file_name = 'current.jpg'
+	f = open(file_name, 'wb')
+	f.write(urllib.urlopen(image_url).read())
 	f.close()
 
-	return fileName
+	return file_name
 
 
-def getPhoto():
-	fileName = downloadPhoto()
+def get_photo():
+	file_name = download_photo()
 
 	print('loading image...')
 
-	img = pygame.image.load(fileName)
-	img = pygame.transform.scale(img, (w, h))
+	with open(file_name, 'r+b') as f:
+		with Image.open(f) as image:
+			cover = resizeimage.resize_cover(image, [w, h])
+			cover.save(file_name, image.format)
 
-	return img
+	image = pygame.image.load(file_name)
+
+	return image
 
 
-def showLoading():
-	loadingImg = pygame.image.load('/app/loading.png')
-	showImage(loadingImg)
+def show_loading():
+	loading_img = pygame.image.load('/app/loading.png')
+	show_image(loading_img)
 
 
-def showImage(image):
+def show_image(image):
 	screen.fill(black)
 	if image is not None:
 		screen.blit(image, (0, 0))
@@ -63,7 +69,7 @@ class Timer:
 		return time.clock() > (self.startTime + self.interval)
 
 
-### START EXECUTION
+# START EXECUTION
 
 sys.stdout = open("/home/pi/logs/adventure_app.log", "w")
 
@@ -80,11 +86,11 @@ screen.fill(black)
 pygame.mouse.set_visible(0)
 pygame.display.set_caption('Adventure.Rocks')
 
-showLoading()
+show_loading()
 
-img = getPhoto()
+img = get_photo()
 
-intervalSeconds = 5 * 60
+intervalSeconds = 1 * 60
 
 timer = Timer()
 timer.start(intervalSeconds)
@@ -101,17 +107,17 @@ while running:
 		if event.type == pygame.MOUSEBUTTONUP:
 			# pos = pygame.mouse.get_pos()
 			print('User wants a new image')
-			showLoading()
-			img = getPhoto()
+			show_loading()
+			img = get_photo()
 			timer.start(intervalSeconds)
 
 	if timer.timerIsUp():
 		print('Rotating to new image')
-		showLoading()
-		img = getPhoto()
+		show_loading()
+		img = get_photo()
 		timer.start(intervalSeconds)
 
-	showImage(img)
+	show_image(img)
 
 pygame.display.quit()
 
