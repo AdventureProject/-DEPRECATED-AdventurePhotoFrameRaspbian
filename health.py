84fad020a806e753671625000f38c4bba055c441
Adam Brown <adamwbrown@gmail.com>
2016-07-18 21:33:56 -0700

    Health monitor now reports the current version as well

--- health.py
+++ health.py
@@ -4,12 +4,16 @@ import urllib2
 import urllib
 
 def get_file_contents(file_path):
-        contents = None
-        with open(file_path, 'r') as file:
-            contents = file.read()
-        return contents
+    contents = None
+    with open(file_path, 'r') as file:
+        contents = file.read()
+    return contents
 
 print('Health check in...')
+
+version = get_file_contents('/app/config/VERSION')
+if version is None:
+    version = 0
 
 errors = get_file_contents('/app/logs/app_errors.log')
 data = urllib.urlencode({'errors' : errors})
@@ -18,7 +22,7 @@ PHOTO_FRAME_ID = get_file_contents( '/app/config/PHOTO_FRAME_ID' )
 if PHOTO_FRAME_ID is None:
 	log('WARNING: PhotoFrame ID is missing!')
 else:
-    url = 'http://wethinkadventure.rocks/health/' + PHOTO_FRAME_ID
+    url = 'http://wethinkadventure.rocks/health/' + PHOTO_FRAME_ID + '?version=' + version
     print(url)
     request = urllib2.Request(url, data)
     response = urllib2.urlopen(request)
